f34734b87d57c0c79d044b6cc081afb86e30c255
Yoon Jae Kim <yj1dreamer@gmail.com>
2019-07-13 15:09:22 +0900

    add files in ch7

--- ch7/fortune-env/fortuneloop.sh
+++ ch7/fortune-env/fortuneloop.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 trap "exit" SIGINT
-INTERVAL=$1
-echo Configured to generate new fortune every $INTERVAL seconds
+echo Configured to generate new fortune ever $INTERVAL seconds
 mkdir -p /var/htdocs
 while :
-do
+do 
   echo $(date) Writing fortune to /var/htdocs/index.html
   /usr/games/fortune > /var/htdocs/index.html
   sleep $INTERVAL
